1f8e11cb440ba3fa9124705c6353b6b16629bd96
Hlautameki <hlautameki@Mateuszs-MacBook-Pro.local>
2018-01-14 13:08:25 +0100

    Tasks list created by JS

--- app.js
+++ app.js
@@ -1,5 +1,53 @@
-var titleChange = function() {
-    document.querySelector('.title').innerHTML = '<h1><b><i>bladd</i></b></h1>';
-}
+// var titleChange = function() {
+//     document.querySelector('.title').innerHTML = '<h1><b><i>bladd</i></b></h1>';
+// }
+(function () {
 
+    var task = function(label) {
+        this.label = label;
+    }
+
+    // var firstElement = {
+    //     label: 'Coffee',
+    // }
+
+    // var secondElement = {
+    //     label: 'Tea',
+    // }
+
+    // var thirdElement = {
+    //     label: 'Milk',
+    // }
+
+    var listElements = [new task('Coffee'), new task('Tea'), new task('Milk')];
+
+    listElements.forEach(element => {
+        addElementToTheList(element);
+    });
+
+    function addElementToTheList(element) {
+        addbulletPointToTheList(element.label);
+    }
+
+    function getList() {
+        return document.getElementsByTagName('ul');
+    }
+
+    function createListElement(text) {
+        var element = document.createElement('li');
+        element.appendChild(document.createTextNode(text));
+        return element;
+    }
+
+    function addbulletPointToTheList(text) {
+        var list = getList();
+        var newElement = createListElement(text);
+        list[0].appendChild(newElement);
+        // console.log(list);
+    }
+
+    addbulletPointToTheList('New element');
+})();
+
+// addbulletPointToTheList('New element');
 // titleChange();
